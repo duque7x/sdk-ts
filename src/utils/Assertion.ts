65f355a29aa37406d7a3eab92c2fd62c71d7c86b
duque das edits <92229705+duque7x@users.noreply.github.com>
2025-07-17 17:05:36 +0100

    refactor: improving typings

--- src/utils/Assertion.ts
+++ src/utils/Assertion.ts
@@ -8,7 +8,7 @@ export class Assertion {
    * Tests if given key is a string
    * @param key Key to test
    */
-  static assertString(key: unknown) {    
+  static assertString(key: unknown): key is string {    
     if (typeof key !== "string") throw new Error(`${key} must be a string`);
     if (!key || key === "") throw new Error(`${key} must be a string`);
     return true;
@@ -18,7 +18,7 @@ export class Assertion {
    * Tests if given key is a number
    * @param key Key to test
    */
-  static assertNumber(key: unknown) {
+  static assertNumber(key: unknown): key is number {
     if (typeof key !== "number") throw new Error(`${key} must be a number`);
     return true;
   }
@@ -27,7 +27,7 @@ export class Assertion {
    * Tests if given key is a boolean
    * @param key Key to test
    */
-  static assertBoolean(key: unknown) {
+  static assertBoolean(key: unknown): key is boolean {
     if (typeof key !== "boolean") throw new Error(`${key} must be a boolean`);
     return true;
   }
@@ -36,7 +36,7 @@ export class Assertion {
    * Tests if given key is an array
    * @param key Key to test
    */
-  static assertArray(key: unknown) {
+  static assertArray(key: unknown): key is object {
     if (!Array.isArray(key)) throw new Error(`${key} must be an array`);
     return true;
   }
@@ -45,7 +45,7 @@ export class Assertion {
    * Tests if given key is an object
    * @param key Key to test
    */
-  static assertObject(key: unknown) {
+  static assertObject(key: unknown): key is object {
     if (typeof key === "object") throw new Error(`${key} must be an object`);
     return true;
   }
